7dcea54adda6bd65451a89899e285965fb39d92e
koojahyeok <gjh7106@snu.ac.kr>
2024-04-02 15:55:55 +0900

    dataset.py

--- dataset.py
+++ dataset.py
@@ -14,41 +14,61 @@ def build_dataset():
 
 class DETRDataset(Dataset):
     def __init__(self, cfg,
-                 pos_img_dir,
-                 neg_img_dir,
+                 img_dir,
                  json_dir,
                  processor):
-        self.pos_img_dir = pos_img_dir
-        self.neg_img_dir = neg_img_dir 
+        self.img_dir = img_dir
         self.json_dir = json_dir
+        self.img_names = sorted(os.listdir(img_dir))
+        self.json_names = sorted(os.listdir(json_dir))
         
-        with open(self.json_dir, "r") as f:
-            self.boxes = json.load(f)
-        self.boxes = {k.split("/")[-1]:v for k, v in self.boxes.items()}
+        # with open(self.json_dir, "r") as f:
+        #     self.boxes = json.load(f)
+        # self.boxes = {k.split("/")[-1]:v for k, v in self.boxes.items()}
+
+        self.boxes = []
+        self.labels = []
+
+        for json_name in sorted(os.listdir(self.json_dir)):
+            path = os.path.join(self.json_dir, json_name)
+
+            with open(path, "r") as f:
+                data = json.load(f)
+                bbox = data["annotations"]["bbox"]
+
+                weed_lbl = data["annoations"]["weeds_kind"]
+                lbl = 0
+
+                if (weed_lbl == 'N'):
+                    label = 0
+                elif (weed_lbl == 'E'):
+                    label = 1
+                elif (weed_lbl == 'A'):
+                    label = 2
+                elif (weed_lbl == 'V'):
+                    label = 3
+                elif (weed_lbl == 'I'):
+                    label = 4
+                elif (weed_lbl == 'B'): 
+                    label = 5
+                else: 
+                    label = 6
             
-        self.pos_img_files = [fn for fn in os.listdir(self.pos_img_dir) if ".json" not in fn]
-        self.neg_img_files = [fn for fn in os.listdir(self.neg_img_dir) if ".json" not in fn]
-        self.pos_img_files.sort()
-        self.neg_img_files.sort()
-        self.img_files = self.pos_img_files + self.neg_img_files
-        self.labels = [0 for _ in range(len(self.pos_img_files))] + [1 for _ in range(len(self.neg_img_files))]
+            self.boxes.append(bbox)
+            self.labels.append(label)
+
+            
+        self.img_files = [fn for fn in sorted(os.listdir(self.img_dir))]
         
         self.img_transform = utils.get_image_transform(cfg, processor)
         self.box_transform = utils.get_box_transform(cfg)
     
     def _load_image(self, image_dir):
-        try:
-            return Image.open(image_dir).convert("RGB")
-        except:
-            return Image.open("/data/jaeyeong/bind_corp/table_ocr/table_detection/synthesized_train_images/00016_840_219.png").convert("RGB")
+        return Image.open(image_dir).convert("RGB")
     
     def __getitem__(self, idx):
-        if self.labels[idx] == 0:
-            img = self._load_image(os.path.join(self.pos_img_dir, self.img_files[idx]))
-            box = self.boxes[self.img_files[idx]]
-        else:
-            img = self._load_image(os.path.join(self.neg_img_dir, self.img_files[idx]))
-            box = [0, 0, 0, 0]
+        img = self._load_image(os.path.join(self.pos_img_dir, self.img_files[idx]))
+        box = self.boxes[idx]
         img_processed = self.img_transform(img)
         label = self.labels[idx]
         
